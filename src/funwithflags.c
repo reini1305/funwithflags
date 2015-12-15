3f10f86f559e9eefd76c5808af81701fe36145a7
Christian Reinbacher <christian.reinbacher@gmx.at>
2015-12-15 20:16:10 +0100

    - Support for FW 3.8 on all platforms

--- src/funwithflags.c
+++ src/funwithflags.c
@@ -33,7 +33,7 @@ static void accel_tap_handler(AccelAxisType axis, int32_t direction) {
 }
 
 static void tickHandler(struct tm *tick_time, TimeUnits units) {
-    updateFlag();
+  updateFlag();
 	static char time_text[] = "00:00";
 	char *time_format;
 	if (clock_is_24h_style()) {
@@ -42,15 +42,17 @@ static void tickHandler(struct tm *tick_time, TimeUnits units) {
 	    time_format = "%I:%M";
 	}
 
-    strftime(time_text, sizeof(time_text), time_format, tick_time);
+  strftime(time_text, sizeof(time_text), time_format, tick_time);
 
-    // Kludge to handle lack of non-padded hour format string
-    // for twelve hour clock.
-    if (!clock_is_24h_style() && (time_text[0] == '0')) {
-      memmove(time_text, &time_text[1], sizeof(time_text) - 1);
-    }
+  // Kludge to handle lack of non-padded hour format string
+  // for twelve hour clock.
+  if (!clock_is_24h_style() && (time_text[0] == '0')) {
+    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+  }
 
-    text_layer_set_text(time_layer, time_text);
+  text_layer_set_text(time_layer, time_text);
+  layer_set_hidden(text_layer_get_layer(time_layer),false);
+  layer_set_hidden(bitmap_layer_get_layer(flags_layer),false);
 }
 
 static void loadWindow(Window *window) {
@@ -73,13 +75,15 @@ static void loadWindow(Window *window) {
     
     flags_layer = bitmap_layer_create(GRect(0,0,bounds.size.w,bounds.size.h-40));
     bitmap_layer_set_alignment(flags_layer,GAlignCenter);
-	updateFlag();
+	  updateFlag();
 	
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
     layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(flags_layer));
     
     layer_set_hidden(text_layer_get_layer(text_layer),true);
+    layer_set_hidden(text_layer_get_layer(time_layer),true);
+    layer_set_hidden(bitmap_layer_get_layer(flags_layer),true);
     accel_tap_service_subscribe(&accel_tap_handler);
     
 }
